fca7272f9fc854f3417a72a7f056657e4e87b761
Birthday_card <kshraddha738@gmail.com>
2023-10-30 20:13:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect,useState } from "react"
+import './App.css'
+const App = () => {
+  const[data,setData]=useState([])
+  const[search,setSearch]=useState("")
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=40363800-8dedda014c5dbc3ed36a9ce85&q=${search}&image_type=photo&pretty=true`)
+    .then(res=>res.json())
+    .then(d=>setData(d.hits))
+  },[data])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <section className="Navbar">
+        <nav className="nav">
+          <div><i class="fa-solid fa-camera"><br /><center>PIXABY</center></i></div>
+          <div>exolore</div>
+          <div><i class="fa-solid fa-bell"></i></div>
+          <button>👆Upload</button>
+        </nav>
+        </section>
 
-export default App;
+
+        <section className="Hea">
+        <header className="Header">
+        <h1>Stunning royalty-free images & royalty-free stock</h1>
+        <h3>over 4.1 milion+ high quality stock, videos and music sheared by our talented community. </h3>
+        <center><input type="text" placeholder="Search Image here What do you want" onChange={(e)=>setSearch(e.target.value)} /></center>
+        </header>
+        </section>
+
+        
+      {data.map((x)=>{
+        return(
+          <section key={x.type} className="secimg">
+            <img src={x.webformatURL} alt=""  height={x.webformatHeight} width={x.webformatWidth}/>
+          </section>
+        )
+     })}
+      
+    </div>
+  )
+}
+export default App
